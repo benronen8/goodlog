cc71bcf5b818e41e36e4809ecccecfa237025627
benronen8 <benronen8@gmail.com>
2025-12-17 21:16:46 +0200

    try remove the hard code templates definition

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,7 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
-templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
